acec3b8347ff5080b3e8063e14e95f94264340e5
resant18 <santoso.renata@gmail.com>
2019-11-28 21:14:52 -0800

    Move sibling with click

--- src/abacus.js
+++ src/abacus.js
@@ -2,27 +2,67 @@
 var sum = [];
 var total = 0;
 var clickMove = "-20px";
+var renderDelay = 100;
 
 function main() {
   var beads = document.getElementsByTagName("td");
   var i;
   for (i = 0; i < beads.length; i++) {
-    beads[i].onclick = changeValue;
+    beads[i].onclick = moveBead;
   }
 }
 
-function changeValue(e) {
-  var value = this.dataset["value"];
-  var state = this.dataset["state"];
-
-  if (state == value) {
+function moveBead(e) {
+  if (isValueSet(this)) {
     // set the element's new position:
-    this.style.top = "";
-    this.dataset["state"] = 0;
+    clearValue(this);
   } else {
-    this.style.top = (-1 * this.clientHeight) +"px";
-    this.dataset["state"] = value;
+    setValue(this);
   }
+}
+
+function clearValue(bead) {
+  var delay = 0;
+  nextSibling = bead.nextElementSibling;
+  if (nextSibling && isValueSet(nextSibling)) {
+    delay = renderDelay + clearValue(nextSibling);
+  }
+  renderClearValue(bead, delay);
+  return delay;
+}
+
+async function renderClearValue(bead, delay) {
+  await sleep(delay);
+  bead.style.top = "";
+  bead.dataset["state"] = 0;
+}
+
+function isValueSet(bead) {
+  var value = bead.dataset["value"];
+  var state = bead.dataset["state"];
+  return value == state;
+}
+
+function setValue(bead, delay) {
+  var delay = 0;
+  prevSibling = bead.previousElementSibling;
+  if (prevSibling && !isValueSet(prevSibling)) {
+    delay = renderDelay + setValue(prevSibling);
+  }
+  renderSetValue(bead, delay);
+  return delay;
+}
+
+async function renderSetValue(bead, delay) {
+  await sleep(delay);
+  bead.style.top = -1 * bead.clientHeight + "px";
+  bead.dataset["state"] = bead.dataset["value"];
+}
+
+async function sleep(msec) {
+  return new Promise(resolve => {
+    setTimeout(resolve, msec);
+  });
 }
 
 sumFunction = function() {
