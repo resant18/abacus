9e117315e37b9bb7d1a5ee6283bf5387f0302fa9
resant18 <santoso.renata@gmail.com>
2019-11-29 17:44:14 -0800

    check value.

--- src/math.js
+++ src/math.js
@@ -1,33 +1,93 @@
+var checkValue = function() {};
+var addend1;
+var addend2;
+var result;
+var total;
+
 function generateMathQuestion(level, operation) {
-   let addend1 = 0;
-   let addend2 = 0;
-   let operator = '';
+  let operator = "";
 
-   let addendEl1 = document.getElementById("addend1");
-   let addendEl2 = document.getElementById("addend2");
-   let operatorEl = document.getElementById("operator");
+  let addendEl1 = document.getElementById("addend1");
+  let addendEl2 = document.getElementById("addend2");
+  let operatorEl = document.getElementById("operator");
 
-   if (operation === 1) operator = '+';
-   else operator = '-';
+  addendEl1.style.color = "white";
+  addendEl2.style.color = "white";
 
-   switch (level) {
-      case 1:
-         addend1 = Math.floor(Math.random() * 5);
-         addend2 = 4 - addend1;
-         break;
-      case 2:
-         addend1 = Math.floor(Math.random() * 10);
-         addend2 = 9 - addend1;
-         break;
-      case 3:
-         addend1 = Math.floor(Math.random() * 10);
-         addend2 = Math.floor(Math.random() * 10);
-         break;
-      default:
-         break;
-   }
+  switch (level) {
+    case 1:
+      addend1 = Math.floor(Math.random() * 5);
+      addend2 = 4 - addend1;
+      break;
+    case 2:
+      addend1 = Math.floor(Math.random() * 10);
+      addend2 = 9 - addend1;
+      break;
+    case 3:
+      addend1 = Math.floor(Math.random() * 10);
+      addend2 = Math.floor(Math.random() * 10);
+      break;
+    default:
+      break;
+  }
 
-   addendEl1.innerHTML = addend1;
-   addendEl2.innerHTML = addend2;
-   operatorEl.innerHTML = operator;
+  if (operation === 1) {
+    operator = "+";
+    result = addend1 + addend2;
+  } else {
+    operator = "-";
+    result = addend1 - addend2;
+  }
+
+  addendEl1.innerHTML = addend1;
+  addendEl2.innerHTML = addend2;
+  operatorEl.innerHTML = operator;
+
+  checkValue = checkAddend1;
+}
+
+function checkAddend1() {
+  if (total === addend1) {
+    addendEl1 = document.getElementById("addend1");
+    addendEl1.style.color = "green";
+    checkValue = checkResult;
+  }
+}
+
+function checkResult() {
+  if (total === result) {
+    addendEl1 = document.getElementById("addend1");
+    addendEl2 = document.getElementById("addend2");
+    operator = document.getElementById("operator");
+    addendEl1.style.color = "green";
+    addendEl2.style.color = "green";
+    operator.style.color = "green";
+    window.alert("good job!");
+  }
+}
+
+function updateSum(sumId, beads) {
+  var sum = 0;
+  for (var i = 0; i < beads.length; i++) {
+    bead = beads[i];
+    if (!isSeparator(bead)) {
+      currentValue = bead.dataset[DATA_CURRENT] || "0";
+      sum += Number(currentValue);
+    }
+  }
+  sumTd = document.getElementById(sumId);
+  sumTd.innerHTML = sum.toString().substring(0, 1);
+  sumTd.dataset["value"] = sum;
+  updateTotalSum();
+}
+
+function updateTotalSum() {
+  total = 0;
+  for (sum of sums) {
+    value = sum.dataset["value"];
+    total = total + Number(value);
+  }
+  document.getElementById("total").innerHTML = "Sum: " + total;
+
+  checkValue();
 }
