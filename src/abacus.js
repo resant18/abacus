6789066376d690bf9c7f0ecfdf74740e3be86b8a
resant18 <santoso.renata@gmail.com>
2020-03-15 20:48:53 -0700

    Update tutorial steps 0-3

--- src/abacus.js
+++ src/abacus.js
@@ -25,6 +25,11 @@ var stepOptions = {
       message: "The top beads are called heaven beads and have value of five.",
       elements: ["message"],
       styles: ["font-size:0.75em"]
+   },
+   4: {
+      message: "To represent numbers, move beads to the center beam. For example, this is 3.",
+      elements: ["message"],
+      styles: ["font-size:0.75em"]
    }
 };
 
@@ -55,8 +60,10 @@ function getBeads() {
 }
 
 function getTdFromTable(tableId, filter = param => true) {
-  unfilteredTds = [...document.getElementById(tableId).querySelectorAll("td")];
-  return unfilteredTds.filter(bead => filter);
+  if (document.getElementById(tableId)) {
+    unfilteredTds = [...document.getElementById(tableId).querySelectorAll("td")];
+    return unfilteredTds.filter(bead => filter);
+  }
 }
 function moveBead(e) {
   if (isValueSet(this)) {
@@ -184,8 +191,7 @@ function setElementsStyle(elements, elementsStyle) {
 }
 
 function showTutorial(step) {
-  let message = document.getElementById("message");
-  message.style.fontSize = "0.75em";
+  let message = document.getElementById("message");  
   
   currentStep += step;
   validationCurrentStep();
@@ -197,8 +203,46 @@ function showTutorial(step) {
     setElementsStyle(stepOption.elements, stepOption.styles);
   }
 
+  let canvas = document.getElementById("canvas");
+  let ctx = canvas.getContext("2d");
+
+  drawTutorial(ctx, currentStep);
+  
 
   console.log(currentStep);
+}
+
+function drawTutorial(ctx, step) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  switch(step) {
+    case 1:
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = "#66ff00";
+      for (let x = 40; x <= 1177; x += 147) {
+         ctx.strokeRect(x, 32, 136, 500);
+      }
+      break;      
+    case 2:
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = "#66ff00";
+      ctx.strokeRect(35, 250, 1175, 265);
+      ctx.fillStyle = "#66ff00";
+      ctx.font = "36px Georgia";
+      ctx.fillText("= 1", 1220, 388);      
+      break;
+    case 3:
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = "#66ff00";
+      ctx.strokeRect(40, 32, 1175, 113);
+      ctx.fillStyle = "#66ff00";
+      ctx.font = "36px Georgia";
+      ctx.fillText("= 5", 1220, 95);
+      break;     
+    case 4:
+      
+    default:
+      break;
+  }
 }
 
 function validationCurrentStep() {
@@ -212,3 +256,4 @@ function validationCurrentStep() {
     prevStep.style.display = "inline-block";    
   }
 }
+
